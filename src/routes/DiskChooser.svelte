b396c7d194dd07edc4ad7929696cffe33604b3df
Gabriel <furstenheim@gmail.com>
2023-08-15 16:44:36 +0200

    Show list of albums

--- src/routes/DiskChooser.svelte
+++ src/routes/DiskChooser.svelte
@@ -37,7 +37,9 @@
   <div class="album-container">
     {#each albums as album (album.id)}
       <div class="album">
-        {album.name}
+        <p class="album-title">
+          {album.name}
+        </p>
       </div>
     {/each}
   </div>
@@ -61,5 +63,46 @@
 
   .error-message {
       color: darkred;
+  }
+
+  .album-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      padding: 4px;
+  }
+
+  .album {
+      flex-grow: 1;
+      flex-basis: calc(20% - 8px) ;
+      display: flex;
+      margin: 2px;
+      padding: 2px 2px 2px 2px;
+      background: lightgrey;
+      border-radius: 2px;
+      border: 1px groove dimgray;
+      height: 40px;
+      text-align: center;
+      justify-content: center;
+  }
+
+  .album:hover {
+      transform: scale(1.01, 1.01);
+  }
+
+  .album-title {
+      align-self: center;
+      font-size: 0.8rem;
+
+      /**Major Properties**/
+      overflow:hidden;
+      line-height: 1rem;
+      max-height: 2rem;
+      -webkit-box-orient: vertical;
+      display: block;
+      display: -webkit-box;
+      text-overflow: ellipsis;
+      -webkit-line-clamp: 2;
+      margin: 0px;
   }
 </style>
